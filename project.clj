803ac2f0ea23ecdb35cc637ab5da95369ea5c64e
Edward Paget <ed.paget@gmail.com>
2014-03-05 15:42:10 +0800

    Clean Files and add license

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject gz-reduction "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache Public License"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.generic "0.1.2"] 
                  [paneer "0.2.0-SNAPSHOT"]
